8cbffa84807b697b42f44b082a8b2d6ff24c1f83
Camila Reyes <careyesla@gmail.com>
2022-07-22 00:02:13 -0400

    Creditos 100% real

--- Scenes/Creditos.gd
+++ Scenes/Creditos.gd
@@ -4,24 +4,24 @@ onready var anim = $PlayerSkinMENU/AnimationPlayer
 onready var timer = get_node("TimerCreditos")
 onready var sprite = $PlayerSkinMENU
 onready var black = $Black
-onready var label = $Label
-onready var credits = $LabelCreditos
+onready var creditsPlayer = $AnimationPlayer
 
 func _ready():
 	black.visible = false
-	label.visible = false
-	credits.visible = true
-	anim.play("Run")
-	timer.set_wait_time(3)
-	timer.start()
-	
-func _process(delta):
-	if run:
-		sprite.position.x += 2
 
-func _on_TimerCreditos_timeout():
+	anim.play("Run")
+	creditsPlayer.play("Desarrolladores")
+	yield(get_tree().create_timer(7),"timeout")
+	
+	creditsPlayer.play("Music")
+	yield(get_tree().create_timer(7),"timeout")
+	
+	creditsPlayer.play("Art")
+	yield(get_tree().create_timer(7),"timeout")
+	
 	black.visible = true
-	label.visible = true
-	credits.visible = false
-	yield(get_tree().create_timer(3),"timeout")
+	creditsPlayer.play("Thanks")
+	yield(get_tree().create_timer(7),"timeout")
 	get_tree().change_scene("res://Scenes/main_menu.tscn")
+	
+	
